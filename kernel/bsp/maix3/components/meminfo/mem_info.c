e408ca0ddcdc63227b8f8a6991b9df71525431dd
kendryte747 <kendryte747@gmail.com>
2024-08-23 13:55:19 +0800

    Add support to get sys_page

--- kernel/bsp/maix3/components/meminfo/mem_info.c
+++ kernel/bsp/maix3/components/meminfo/mem_info.c
@@ -1,3 +1,4 @@
+#include "lwp_user_mm.h"
 #include <dfs_file.h>
 #include <rtthread.h>
 #include <string.h>
@@ -5,6 +6,8 @@
 
 #ifdef RT_USING_LWP
 #include <lwp.h>
+#include <page.h>
+#include <mmu.h>
 #endif
 #ifndef ARCH_PAGE_SIZE
 #define ARCH_PAGE_SIZE 0
@@ -13,93 +16,83 @@
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-typedef struct
-{
-    rt_list_t *list;
-    rt_list_t **array;
-    rt_uint8_t type;
-    int nr;             /* input: max nr, can't be 0 */
-    int nr_out;         /* out: got nr */
+typedef struct {
+  rt_list_t *list;
+  rt_list_t **array;
+  rt_uint8_t type;
+  int nr;     /* input: max nr, can't be 0 */
+  int nr_out; /* out: got nr */
 } list_get_next_t;
 
-static void list_find_init(list_get_next_t *p, rt_uint8_t type, rt_list_t **array, int nr)
-{
-    struct rt_object_information *info;
-    rt_list_t *list;
+static void list_find_init(list_get_next_t *p, rt_uint8_t type,
+                           rt_list_t **array, int nr) {
+  struct rt_object_information *info;
+  rt_list_t *list;
 
-    info = rt_object_get_information((enum rt_object_class_type)type);
-    list = &info->object_list;
+  info = rt_object_get_information((enum rt_object_class_type)type);
+  list = &info->object_list;
 
-    p->list = list;
-    p->type = type;
-    p->array = array;
-    p->nr = nr;
-    p->nr_out = 0;
+  p->list = list;
+  p->type = type;
+  p->array = array;
+  p->nr = nr;
+  p->nr_out = 0;
 }
 
-static rt_list_t *list_get_next(rt_list_t *current, list_get_next_t *arg)
-{
-    int first_flag = 0;
-    rt_ubase_t level;
-    rt_list_t *node, *list;
-    rt_list_t **array;
-    int nr;
+static rt_list_t *list_get_next(rt_list_t *current, list_get_next_t *arg) {
+  int first_flag = 0;
+  rt_ubase_t level;
+  rt_list_t *node, *list;
+  rt_list_t **array;
+  int nr;
 
-    arg->nr_out = 0;
+  arg->nr_out = 0;
 
-    if (!arg->nr || !arg->type)
-    {
-        return (rt_list_t *)RT_NULL;
+  if (!arg->nr || !arg->type) {
+    return (rt_list_t *)RT_NULL;
+  }
+
+  list = arg->list;
+
+  if (!current) /* find first */
+  {
+    node = list;
+    first_flag = 1;
+  } else {
+    node = current;
+  }
+
+  level = rt_hw_interrupt_disable();
+
+  if (!first_flag) {
+    struct rt_object *obj;
+    /* The node in the list? */
+    obj = rt_list_entry(node, struct rt_object, list);
+    if ((obj->type & ~RT_Object_Class_Static) != arg->type) {
+      rt_hw_interrupt_enable(level);
+      return (rt_list_t *)RT_NULL;
     }
+  }
 
-    list = arg->list;
+  nr = 0;
+  array = arg->array;
+  while (1) {
+    node = node->next;
 
-    if (!current) /* find first */
-    {
-        node = list;
-        first_flag = 1;
+    if (node == list) {
+      node = (rt_list_t *)RT_NULL;
+      break;
     }
-    else
-    {
-        node = current;
+    nr++;
+    *array++ = node;
+    if (nr == arg->nr) {
+      break;
     }
+  }
 
-    level = rt_hw_interrupt_disable();
-
-    if (!first_flag)
-    {
-        struct rt_object *obj;
-        /* The node in the list? */
-        obj = rt_list_entry(node, struct rt_object, list);
-        if ((obj->type & ~RT_Object_Class_Static) != arg->type)
-        {
-            rt_hw_interrupt_enable(level);
-            return (rt_list_t *)RT_NULL;
-        }
-    }
-
-    nr = 0;
-    array = arg->array;
-    while (1)
-    {
-        node = node->next;
-
-        if (node == list)
-        {
-            node = (rt_list_t *)RT_NULL;
-            break;
-        }
-        nr++;
-        *array++ = node;
-        if (nr == arg->nr)
-        {
-            break;
-        }
-    }
-
-    rt_hw_interrupt_enable(level);
-    arg->nr_out = nr;
-    return node;
+  rt_hw_interrupt_enable(level);
+  arg->nr_out = nr;
+  return node;
 }
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
@@ -110,7 +103,8 @@ static int meminfo_open(struct dfs_fd *file) { return 0; }
 
 static int meminfo_close(struct dfs_fd *file) { return 0; }
 
-#define MEMINFO_DEV_CMD_READ (0x1024)
+#define MEMINFO_DEV_CMD_READ_HEAP (0x1024 + 0)
+#define MEMINFO_DEV_CMD_READ_PAGE (0x1024 + 1)
 
 struct meminfo_t {
   size_t total_size;
@@ -120,8 +114,8 @@ struct meminfo_t {
 
 static int meminfo_ioctl(struct dfs_fd *file, int cmd, void *args) {
   switch (cmd) {
-  case MEMINFO_DEV_CMD_READ:
-    struct meminfo_t *meminfo = (struct meminfo_t *)args;
+  case MEMINFO_DEV_CMD_READ_HEAP:
+    struct meminfo_t meminfo;
     size_t _total = 0, _free = 0, _used = 0;
 
 #ifdef RT_USING_MEMHEAP_AS_HEAP
@@ -178,11 +172,27 @@ static int meminfo_ioctl(struct dfs_fd *file, int cmd, void *args) {
     _free = _total - _used;
 #endif // RT_USING_MEMHEAP_AS_HEAP
 
-    meminfo->total_size = _total;
-    meminfo->free_size = _free;
-    meminfo->used_size = _used;
+    meminfo.total_size = _total;
+    meminfo.free_size = _free;
+    meminfo.used_size = _used;
 
+    if(sizeof(struct meminfo_t) != lwp_put_to_user(args, &meminfo, sizeof(struct meminfo_t))) {
+        rt_kprintf("put_to_user failed 0x%08X\n", cmd);
+    }
     break;
+  case MEMINFO_DEV_CMD_READ_PAGE: {
+    struct meminfo_t meminfo;
+    size_t total, free;
+    rt_page_get_info(&total, &free);
+
+    meminfo.total_size = total * PAGE_SIZE;
+    meminfo.free_size = free * PAGE_SIZE;
+    meminfo.used_size = meminfo.total_size - meminfo.free_size;
+
+    if(sizeof(struct meminfo_t) != lwp_put_to_user(args, &meminfo, sizeof(struct meminfo_t))) {
+        rt_kprintf("put_to_user failed 0x%08X\n", cmd);
+    }
+  } break;
   default:
     rt_kprintf("unknown cmd 0x%x\n", cmd);
     return -1;
@@ -218,4 +228,3 @@ int meminfo_device_init(void) {
   return 0;
 }
 INIT_DEVICE_EXPORT(meminfo_device_init);
-
